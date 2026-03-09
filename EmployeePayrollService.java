ae1baa7fe7b34a5150be2992195ac7d1189367dd
Tanmay <tanmaytanvi1234@gmail.com>
2026-03-09 23:44:30 +0530

    [Tanmay] : Added UC2 JDBC implementation to retrieve employee payroll data from database

--- EmployeePayrollService.java
+++ EmployeePayrollService.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Enumeration;
+import java.sql.*;
 
 public class EmployeePayrollService {
 
@@ -12,18 +10,21 @@ public class EmployeePayrollService {
 
         try {
 
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Driver Loaded");
-
-            Enumeration<java.sql.Driver> drivers = DriverManager.getDrivers();
-
-            while (drivers.hasMoreElements()) {
-                System.out.println("Registered Driver: " + drivers.nextElement());
-            }
-
             Connection connection = DriverManager.getConnection(url, user, password);
 
-            System.out.println("Connection Established");
+            Statement statement = connection.createStatement();
+
+            ResultSet rs = statement.executeQuery("SELECT * FROM employee_payroll");
+
+            while (rs.next()) {
+
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                double salary = rs.getDouble("salary");
+                Date startDate = rs.getDate("start_date");
+
+                System.out.println(id + " " + name + " " + salary + " " + startDate);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
